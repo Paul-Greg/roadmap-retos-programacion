5ad19d6f5bfced23e70724b6cb955345cc280046
Paul-Greg <154803592+Paul-Greg@users.noreply.github.com>
2024-06-25 17:00:59 +0200

    Create Paul-Greg.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Paul-Greg.java
@@ -0,0 +1,37 @@
+package retoscode.retoscode;
+
+public class RetosCode {
+
+    //Java: https://www.java.com/es/
+    //Comentario en una línea
+    /*Comentario en
+    varias
+    líneas
+     */
+    /**
+     * Comentarios de documentación (javadoc) Se usa para generar documentación
+     * API. Se coloca antes de clases, metodos y campos para describir su
+     * proposito y comportamiento.
+     */
+    //Variable
+    int num = 0;
+
+    //Constante
+    public static int CONSTANTE = 10;
+
+    //Variables con datos primitivos
+    String cadena = "paul";
+    byte numPequeno = 127;
+    short numMenosPeque = 455;
+    int num1 = 65456415;
+    long numLargo = 654897494;
+    float coma = 1.54f;
+    double doble = 3.40282;
+    char letra = 'g';
+    boolean facts = true;
+    
+    public static void main(String[] args) {
+        System.out.println("¡Hola, Java!");
+    }
+
+}
